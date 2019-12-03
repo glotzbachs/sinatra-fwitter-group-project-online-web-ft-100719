eadbb8aed95696cd77377766e1b6acb83d1bab29
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-12-03 13:23:31 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,50 @@
 class UsersController < ApplicationController
 
+    # get '/users/'
+
+    get '/signup' do
+        if logged_in?
+            redirect '/tweets'
+        else
+            erb :'/users/signup'
+        end
+
+    end
+
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            @success="no"
+            erb :'/users/signup'
+        else    
+            @user=User.new(username: params[:username], email: params[:email], password: params[:password])
+            redirect '/login'
+        end
+    end
+
+    get '/login' do
+        if logged_in?
+            redirect '/tweets'
+        else
+            erb :'/users/login'
+        end
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[:username])
+	    if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/tweets"
+		else
+			redirect "/login"
+        end
+    end
+
+    get "/logout" do
+        session.clear
+        redirect "/"
+      end
+
+
+
 
 end
